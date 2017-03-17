4110ac0cafb75ae4039dddb144aa63f307fc5d3f
Matthew Moore <mmoore@globalpersonals.co.uk>
2017-03-17 08:14:01 +0000

    Uses Chrome as defaut browser

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,17 +7,11 @@ require "site_prism"
 Dir[File.dirname(__FILE__) + "/../../library/*.rb"].each { |file| require file }
 
 Capybara.configure do |config|
-  config.default_driver = :firefox
-  config.javascript_driver = :firefox
-  config.run_server = false
-  config.default_selector = :css
-  config.default_wait_time = 5
-  config.match = :prefer_exact
-  config.ignore_hidden_elements = false
+  config.default_driver = :chrome
 end
 
-Capybara.register_driver :firefox do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :firefox)
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
 def desktop(options = {})
